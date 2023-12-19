8747e09be2470fcf5b130a36293883aeec48fe23
161043261 <yukino161043261@gmail.com>
2023-12-19 08:28:12 +0800

    Dec 19th

--- 2020.ts
+++
@@ -1,7 +0,0 @@
-function arrMax(arr: number[], begin: number): number {
-    if (begin == arr.length - 1) return arr[begin]
-    return Math.max(arr[begin], arrMax(arr, begin + 1))
-}
-
-let arr = [1, 6, 1, 0, 4, 3, 2, 6, 1]
-console.log(arrMax(arr, 0))
